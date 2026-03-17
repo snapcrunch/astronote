158105c78b80ba3b2eeaf901daa4e34cbbd89ece
Tim Ambler <tkambler@gmail.com>
2026-03-16 20:19:26 -0500

    misc

--- packages/domain/src/actions/collections/remove.ts
+++ packages/domain/src/actions/collections/remove.ts
@@ -9,6 +9,8 @@ export async function remove(user: AuthUser, id: number): Promise<boolean> {
   const remaining = await repository.getCollections(user.id);
   if (remaining.length === 0) {
     await populateUser(user);
+  } else if (remaining.length === 1) {
+    await repository.setDefaultCollection(user.id, remaining[0].id);
   }
 
   return true;
