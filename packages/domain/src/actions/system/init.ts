f03f35a54705c2cf524550ff3acd346e29709385
Tim Ambler <tkambler@gmail.com>
2026-03-20 13:21:15 -0500

    -console.log

--- packages/domain/src/actions/system/init.ts
+++ packages/domain/src/actions/system/init.ts
@@ -20,8 +20,6 @@ const init = async ({
 
   const settings = await domain.systemSettings.get();
 
-  console.log('defaultPassword', defaultPassword);
-
   if (!settings.initialized) {
     if (defaultUsername && defaultPassword) {
       await domain.users.createUser(defaultUsername, defaultPassword);
